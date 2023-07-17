bc1aadee1ddcedaa35014a01e4e04e99c61b8030
darker <darkerros@yandex.ru>
2023-07-18 01:38:43 +0300

    feat: create token storage utils

---
+++ src/utils/token-storage.ts
@@ -0,0 +1,18 @@
+import {ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY} from "../consts/token-keys";
+
+export const getAccessToken = (): string | null => localStorage.getItem(ACCESS_TOKEN_KEY)
+export const setAccessToken = (value: string | null) => {
+  if (value) {
+    localStorage.setItem(ACCESS_TOKEN_KEY, value)
+    return;
+  }
+  localStorage.removeItem(ACCESS_TOKEN_KEY)
+}
+export const setRefreshToken = (value: string | null) => {
+  if (value) {
+    localStorage.setItem(REFRESH_TOKEN_KEY, value)
+    return;
+  }
+  localStorage.removeItem(REFRESH_TOKEN_KEY)
+}
+export const getRefreshToken = (): string | null => localStorage.getItem(REFRESH_TOKEN_KEY)
