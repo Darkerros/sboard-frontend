c9496ff5cbd2002e7145f02b24d1747ef9163f46
darker <darkerros@yandex.ru>
2023-07-19 02:26:30 +0300

    feat: add to app routes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { styled } from "styled-components";
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "../pages/login-page/login-page";
+import RegisterPage from "../pages/register-page/register-page";
+import PostControllPage from "../pages/post-controll-page/post-controll-page";
+import {checkAuthThunk} from "../services/thunks/check-auth-thunk";
+import {useAppDispatch} from "../hooks/use-app-dispatch";
+import ProtectedRoute from "../routes/protected-route/protected-route";
+import Modals from "../modules/modals/modals";
 
 const AppContent = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
 
   display: flex;
   justify-content: center;
@@ -13,9 +21,20 @@ const AppContent = styled.div`
 `
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(checkAuthThunk())
+  },[dispatch])
 
   return (
     <AppContent>
+      <Routes>
+        <Route path={'/login'} element={<ProtectedRoute needAuth={false} redirectUrl={'/'}><LoginPage/></ProtectedRoute>}/>
+        <Route path={'/register'} element={<ProtectedRoute needAuth={false} redirectUrl={'/'}><RegisterPage/></ProtectedRoute>}/>
+        <Route path={'/'} element={<ProtectedRoute needAuth={true} redirectUrl={'/login'}><PostControllPage/></ProtectedRoute>}/>
+      </Routes>
+      <Modals/>
     </AppContent>
   );
 }
